d67faf94ff281cf2f13228716c4847ca21a4f505
lenilsamuel <lenilvarghese@gmail.com>
2021-12-11 10:04:57 -0700

    add form validation to not allow empty submission or submission greater than 140 characters

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,31 +5,6 @@
  */
 
 $(document).ready(function () {
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ];
-
   const renderTweets = (tweets) => {
     $("#tweets-container").empty();
     for (const tweet of tweets) {
@@ -74,6 +49,13 @@ $(document).ready(function () {
 
   $('#tweet-form').submit(function (event) {
     event.preventDefault();
+    const tweetLength = $('#tweet-text').val().length;
+    console.log(tweetLength);
+    if (!tweetLength) {
+      return alert('Tweet cannot be empty');
+    } if (tweetLength > 140) {
+      return alert('Only 140 characters allowed, sorry. Please shorten tweet');
+    }
     const serializedData = $(this).serialize();
     $.post('/tweets', serializedData).then(()=> {
       loadTweets();
