dcc9b041bc4e611bcdc438d0caf3ac2c2e99f91d
lenilsamuel <lenilvarghese@gmail.com>
2022-01-06 19:45:36 -0700

    add comments to code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,14 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
+  // function that, on user input, changes the tweet counter and displays the number of characters remaining
   let maxChars = 140;
-  $('#tweet-text').on('input', function(event) {
+
+  $("#tweet-text").on("input", function (event) {
     let remaining = maxChars - $(this).val().length;
-    $(this).parent().find('.counter').html(remaining);
+    $(this).parent().find(".counter").html(remaining);
+
+    // changes tweet counter to red if tweets go over max 140 limit
     if (remaining < 0) {
-      $('.counter').addClass('remaining');
-    } else $('.counter').removeClass('remaining');
-  })
-}); 
+      $(".counter").addClass("remaining");
+    } else $(".counter").removeClass("remaining");
+  });
+});
